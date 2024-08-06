e06ae62ddbbb2e5ef891dc147189ecf012f2f657
youngeun Chae <dlalsgur1625@naver.com>
2024-08-06 23:23:01 +0900

    [add] signup (isNew)

--- emolog-app/src/page/SignUp/SignUp.jsx
+++ emolog-app/src/page/SignUp/SignUp.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import KakaoLogin from "./KakaoLogin";
 import "./SignUp.css";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [isNew, setIsNew] = useState(null);
+
+  useEffect(() => {
+
+    const checkIsNew = () => {
+      const isNewUser = localStorage.getItem("isNew") === "true";
+      setIsNew(isNewUser);
+    };
+
+    checkIsNew();
+  }, []);
+
+  useEffect(() => {
+ 
+    if (isNew !== null) {
+      if (isNew) {
+        navigate("/userform");
+      } else {
+        navigate("/monthly");
+      }
+    }
+  }, [isNew, navigate]);
+
   return (
     <div className="SignUp">
       <div className="container">
@@ -10,10 +35,14 @@ const Signup = () => {
           <div id="logo"></div>
           <div id="sign_message"></div>
           <div id="kakao">
-            <KakaoLogin />
+            <KakaoLogin  />
           </div>
-          <a href="https://emolog.kro.kr/login/oauth2/code/naver"><div id="naver" ></div></a>
-          <a href="https://emolog.kro.kr/login/oauth2/code/google"><div id="google"></div></a>
+          <a href="https://emolog.kro.kr/login/oauth2/code/naver">
+            <div id="naver" onClick={Signup}></div>
+          </a>
+          <a href="https://emolog.kro.kr/login/oauth2/code/google">
+            <div id="google" onClick={Signup}></div>
+          </a>
           <div id="copyright"></div>
         </div>
       </div>
